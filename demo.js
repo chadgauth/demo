2e2cb9979d884925c5748a8b89605f690b30d256
chadgauth <chadgauth@gmail.com>
2015-04-19 16:25:04 -0600

    Update demo.js

--- demo.js
+++ demo.js
@@ -5,6 +5,34 @@ $(window).scroll(function() {
       $("header").addClass("shadow");
     }
 });
+
+window.onload = function () {
+    if (typeof history.pushState === "function") {
+        history.pushState("jibberish", null, null);
+        window.onpopstate = function () {
+            history.pushState('newjibberish', null, null);
+            // Handle the back (or forward) buttons here
+            // Will NOT handle refresh, use onbeforeunload for this.
+        };
+    }
+    else {
+        var ignoreHashChange = true;
+        window.onhashchange = function () {
+            if (!ignoreHashChange) {
+                ignoreHashChange = true;
+                window.location.hash = Math.random();
+                // Detect and redirect change here
+                // Works in older FF and IE9
+                // * it does mess with your hash symbol (anchor?) pound sign
+                // delimiter on the end of the URL
+            }
+            else {
+                ignoreHashChange = false;   
+            }
+        };
+    }
+};
+
 $(".well").click(function() {
   	var div = "#"+this.id.toString();
   	$('html, body').animate({scrollTop: $(div).offset().top-61}, 200);
